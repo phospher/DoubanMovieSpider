bb9d9f42d68626b4ed6662a755b05d4e91c3219d
phospher <phospher@qq.com>
2024-03-03 11:39:59 +0800

    adding latest movie scores to email body and avaliable to open movie page in Douban

--- mail/main.py
+++ mail/main.py
@@ -6,15 +6,29 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 import os
 import logging
+import sqlite3
+import datetime
 
 RESULT_DIR = './result'
 EMAIL_TITLE = "豆瓣每日日报"
-EMAIL_FROM = "**"
-EMAIL_PASSWORD = "**"
-EMAIL_TO = ["**"]
-EMAIL_HOST = "**"
+EMAIL_FROM = "***"
+EMAIL_PASSWORD = "***"
+EMAIL_TO = ["***"]
+EMAIL_HOST = "***"
 EMAIL_PORT = 465
-EMAIL_MESSAGE = "豆瓣每日日报见附件"
+EMAIL_MESSAGE = '''
+<html>
+    <head>
+    </head>
+    <body>
+        <p>最新电影评分：</p>
+        <ul>
+            {}
+        </ul>
+        <p>评分趋势见附件</p>
+    </body>
+</html>
+'''
 
 
 def init_log():
@@ -23,9 +37,20 @@ def init_log():
     return logging.getLogger()
 
 
-def read_message_content():
+def read_message_attachment():
     with open(os.path.join(RESULT_DIR, "latest.html")) as file:
         return file.read()
+
+
+def read_message_content():
+    data_list_html = ""
+    with sqlite3.connect('douban.db') as conn:
+        cur = conn.cursor()
+        cur.execute(
+            " select distinct subject, title, socre from movie where strftime('%Y/%m/%d', createdtime)=? order by socre desc", (datetime.datetime.now().strftime('%Y/%m/%d'),))
+        for item in cur.fetchall():
+            data_list_html += f"<li><a href=\"https://movie.douban.com/subject/{item[0]}/\" target=\"_blank\">{item[1]}：{item[2]}</a></li>"
+    return EMAIL_MESSAGE.format((data_list_html))
 
 
 def send_email():
@@ -33,9 +58,9 @@ def send_email():
     message["Subject"] = Header(EMAIL_TITLE)
     message["From"] = Header(EMAIL_FROM)
     message["To"] = Header(";".join(EMAIL_TO))
-    message.attach(MIMEText(EMAIL_MESSAGE, "plain", "utf-8"))
+    message.attach(MIMEText(read_message_content(), "html", "utf-8"))
 
-    attachment = MIMEText(read_message_content(), 'base64', 'utf-8')
+    attachment = MIMEText(read_message_attachment(), 'base64', 'utf-8')
     attachment["Content-Type"] = "application/octet-stream"
     attachment["Content-Disposition"] = 'attachment; filename="latest.html"'
     message.attach(attachment)
